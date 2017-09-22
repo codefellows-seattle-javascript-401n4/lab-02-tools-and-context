58ac36d572f0a79162704e5d69ad36cdd0bf476d
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-09-21 20:51:45 -0700

    bind tested/working

--- test/fp.test.js
+++ test/fp.test.js
@@ -3,14 +3,26 @@
 const fp = require('../lib/fp.js');
 const expect = require('expect');
 
-describe('fp.map', function(){
+describe('fp.mapCall', function(){
     it('should square every number in the original array', function(){
-        expect(fp.map([1,2,3], n => n*n)).toEqual([1,4,9]);
+        expect(fp.mapCall([1,2,3], n => n*n)).toEqual([1,4,9]);
     });
 });
 
 describe('fp.map', function(){
     it('should add a space and poop to every string', function (){
-        expect(fp.map(['smelly', 'stinky', 'huuuuuge'], n => `${n} poop`)).toEqual(['smelly poop', 'stinky poop', 'huuuuuge poop'])
+        expect(fp.mapCall(['smelly', 'stinky', 'huuuuuge'], n => `${n} poop`)).toEqual(['smelly poop', 'stinky poop', 'huuuuuge poop'])
+    })
+})
+
+describe('fp.mapBind', function(){
+    it('should return a new function that will in turn square every number in original array', function(){
+        expect(fp.mapBind([1,2,3], n => n*n)).toEqual([1,4,9]);
+    });
+});
+
+describe('fp.mapBind', function(){
+    it('should return a new funciton that will in turn then add a space and poop to every string', function (){
+        expect(fp.mapBind(['smelly', 'stinky', 'huuuuuge'], n => `${n} poop`)).toEqual(['smelly poop', 'stinky poop', 'huuuuuge poop'])
     })
 })
