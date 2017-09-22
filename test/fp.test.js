a00c196c52be0159967ff9e2dbd9a55f2a5610f1
meganr <meganrm@gmail.com>
2017-09-21 21:39:18 -0700

    made functions that take array methods as params and returns a new function

---
+++ test/fp.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const expect = require('expect');
+const arrayFunctions = require('../lib/fp.js');
+
+// describe('Array Functions', function(){
+  describe('Array call function maker', function(){
+    it('takes an array method and makes a new function using call', function(){
+      let result = arrayFunctions.arrayCall('map')
+      expect(result).to.be.an('function');
+    })
+  })
+
+// })
