15f0bb7d6afc16519407cfed462e14aa375e94be
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-09-21 21:24:32 -0700

    done for thursday

--- test/fp.test.js
+++ test/fp.test.js
@@ -3,6 +3,16 @@
 const fp = require('../lib/fp.js');
 const expect = require('expect');
 
+let isEven = function(arr){
+    let evens = [];
+    arr.forEach(function(element) {
+        return element%2 === 0 ? evens.push(element)
+    }, this);
+
+
+
+}
+
 describe('fp.mapCall', function(){
     it('should square every number in the original array', function(){
         expect(fp.mapCall([1,2,3], n => n*n)).toEqual([1,4,9]);
@@ -29,7 +39,7 @@ describe('fp.mapBind', function(){
 
 describe('fp.mapApply', function(){
     it('should square every number in the original array', function (){
-        expect(fp.mapApply([1,2,3], [n => n*n])).toEqual([1,4,9]);
+        expect(fp.mapApply([1,2,3], ).toEqual([1,4,9]);
     })
 })
 
@@ -38,3 +48,9 @@ describe('fp.mapApply', function(){
         expect(fp.mapApply(['smelly', 'stinky', 'huuuuuge'], [n => `${n} poop`])).toEqual(['smelly poop', 'stinky poop', 'huuuuuge poop'])
     })
 })
+
+describe('fp.filterCall', function(){
+    it('should take an array and return a new array with only the even numbers', function(){
+        expect(fp.filterCall([2,3,4,5,6], n => return n%2 ===0 ? ))
+    })
+});
