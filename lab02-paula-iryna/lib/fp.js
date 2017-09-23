221dd0df8a4690fbfb23fd14952b1c521fc2ffef
Iryna Maslova <maslovai@hotmail.com>
2017-09-22 22:40:30 -0700

    work two thirds done.  figured out how to bind to map, need to fix apply in every method

--- lab02-paula-iryna/lib/fp.js
+++ lab02-paula-iryna/lib/fp.js
@@ -3,21 +3,31 @@ const fp = module.exports = {};
 
 fp.mapCall = ((list, callback) => Array.prototype.map.call(list, callback));
 fp.mapApply = ((list, callback) => Array.prototype.map.apply(list, callback));
-// fp.mapBind = function(list, callback){
-//   let method = function(list, callback) {
-//     return Array.prototype.filter.call(this.n, this.cb);};
-//   let obj = {n:numbers, cb:mapwith};
-//   return method.bind(obj);
-// };
 
-// fp.mapBind =(() => Array.prototype.filter.call(this.n, this.cb)).bind({n:numbers, cb:mapwith});
-// console.log(fp.mapBind);
+fp.getMapBoundFunction = function() {
+  let obj ={
+    n : [2,6,13,20,55],
+    cb:(a=>a>15),
+  };
+  // Define a function
+  let method =function(){
+    return Array.prototype.filter.call(this.n, this.cb);
+  };
+  // Bind it
+  let mapBound = method.bind(obj);
+
+  // Return bound function
+  return mapBound;
+  //console.log( mapBound);
+};
+
+
 fp.filterCall = ((list, callback)=> Array.prototype.filter.call(list, callback));
 fp.filterApply = ((list, callback)=> Array.prototype.filter.apply(list, callback));
-// fp.filterBind = ((list, callback)=> Array.prototype.filter.call(list, callback).bind());
+
 
 fp.reduceCall = ((list, callback) => Array.prototype.reduce.call(list, callback));
-fp.mapApply = ((list, callback) => Array.prototype.ma.apply(list, callback));
+fp.mapApply = ((list, callback) => Array.prototype.reduce.apply(list, callback));
 // fp.mapBind =((list, callback) => Array.prototype.filter.call(list, callback)).bind();
 
 fp.spliceCall = ((list,start) => Array.prototype.splice.call(list, start));
