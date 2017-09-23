44bd36867691bd3dfa0e9ee360d1aebf45027efb
Sung Kim <sng.kim103@gmail.com>
2017-09-23 09:14:13 -0700

    response

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const fp = require('./lib/fp.js');
 
 let input_array = process.argv.splice(2, process.argv.length);
 
+if (input_array.length === 0){
+	console.log('please enter node index.js arg1 arg2 arg3... to get a response');
+}
+
 let result = fp.map.call(input_array, el => el.toUpperCase())
 
 console.log(result);
