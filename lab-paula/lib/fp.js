ca2d96e0f3a2344466e8dbc020968d00525f5cda
Pmookerjee <pmookerjee@gmail.com>
2017-09-23 01:00:22 -0700

    Recreated my repo in a new location due to a dir error - thus lumping all commits into this one.

---
+++ lab-paula/lib/fp.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const fp = module.exports = {};
+
+let arr = [ 0, 15, 1 ];
+let arr2 = [ 8, 99, 10, 3, 4];
+let index = 5;
+let total, current = 0;
+let mapwith = n => n+3;
+let filterCriteria = a => a < 13;
+
+let isValidArray = (input => {
+  return (!Array.isArray(input)) ? false :
+   input.every(x => {return typeof x === 'number'; });
+});
+
+/******  FILTER ******/
+
+fp.filterWithCall = (list, callback) => {
+  return isValidArray(list) ? Array.prototype.filter.call(list, callback) : null;
+};
+
+fp.filterWithApply = (list, callback) => {
+  return isValidArray(list) ? Array.prototype.filter.apply(list, callback) : null;
+};
+
+fp.filterWithBind = function() {
+  if(!isValidArray(this.list)) return null;
+  else return Array.prototype.filter.call(this.list, this.callback);
+}.bind({ list:arr, callback:filterCriteria });
+
+/******  MAP ******/
+
+fp.mapWithCall = (list, callback) => {
+  return isValidArray(list) ? Array.prototype.map.call(list, callback) : null;
+};
+
+fp.mapWithApply = (list, callback) => {
+  return isValidArray(list) ? Array.prototype.map.apply(list, callback) : null;
+};
+
+fp.mapWithBind = function() {
+  if(!isValidArray(this.list)) return null;
+  else return Array.prototype.map.call(this.list, this.callback);
+}.bind( {list:arr, callback:mapwith});
+
+
+/******  CONCAT ******/
+
+fp.concatWithCall = (list, list2) => {
+  return (isValidArray(list) && isValidArray(list2)) ? Array.prototype.concat.call(list, list2) : null;
+};
+
+fp.concatWithApply = (list, list2) => {
+  return (isValidArray(list) && isValidArray(list2)) ? Array.prototype.concat.apply(list, list2) : null;
+};
+
+fp.concatWithBind = function() { return Array.prototype.concat.call(this.list, this.list2);}.bind( {list:arr, list2:arr2});
+
+
+/******  SPLICE ******/
+
+fp.spliceWithCall = (list, start) => {
+  return isValidArray(list) ? Array.prototype.splice.call(list, start) : null;
+};
+
+fp.spliceWithApply = (list, start) => {
+  return (isValidArray(list) && isValidArray(start)) ? Array.prototype.splice.apply(list, start) : null;
+};
+
+fp.spliceWithBind = function () { return Array.prototype.splice.call(this.list, this.start);}.bind({ list:arr, start:index});
+
+
+/******  REDUCE ******/
+
+fp.reduceWithCall = (list, curr) => {
+  return isValidArray(list) ? Array.prototype.reduce.call(list, curr) : null;
+};
+
+fp.reduceWithApply = (list, curr) => {
+  return isValidArray(list) ? Array.prototype.reduce.apply(list, curr) : null;
+};
+
+// fp.reduceWithBind = function() {
+//   return Array.prototype.reduce.call(this.prev, this.curr);
+// }.bind({prev:total, curr:current});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fp.filterWithCall(arr, a=>a<13);
+fp.filterWithApply(arr, [a=>a<13]);
