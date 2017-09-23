ee16806807fb77af37f80b73c8f61f559a851102
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-09-22 18:02:13 -0700

    break to go get some DTF

--- test/fp.test.js
+++ test/fp.test.js
@@ -3,54 +3,86 @@
 const fp = require('../lib/fp.js');
 const expect = require('expect');
 
-let isEven = function(arr){
-    let evens = [];
-    arr.forEach(function(element) {
-        return element%2 === 0 ? evens.push(element)
-    }, this);
+// helper arrays
+
+let goodNums = [1,2,3,4,5,6];
+let shitNums = [6,9, 'poop'];
+let moreNums = [7,8,9];
 
 
-
-}
+// map tests
 
 describe('fp.mapCall', function(){
-    it('should square every number in the original array', function(){
-        expect(fp.mapCall([1,2,3], n => n*n)).toEqual([1,4,9]);
+    it('should take the given array and square every number in it', function(){
+        expect(fp.mapCall(moreNums, n => n*n)).toEqual([49,64,81]);
     });
 });
 
 describe('fp.mapCall', function(){
-    it('should add a space and poop to every string', function (){
-        expect(fp.mapCall(['smelly', 'stinky', 'huuuuuge'], n => `${n} poop`)).toEqual(['smelly poop', 'stinky poop', 'huuuuuge poop'])
-    })
-})
-
-describe('fp.mapBind', function(){
-    it('should return a new function that will in turn square every number in original array', function(){
-        expect(fp.mapBind([1,2,3], n => n*n)).toEqual([1,4,9]);
+    it('should return null if the array given isn\'t all numbers', function(){
+        expect(fp.mapCall(shitNums, n => n*n)).toBe(null);
     });
 });
 
 describe('fp.mapBind', function(){
-    it('should return a new funciton that will in turn then add a space and poop to every string', function (){
-        expect(fp.mapBind(['smelly', 'stinky', 'huuuuuge'], n => `${n} poop`)).toEqual(['smelly poop', 'stinky poop', 'huuuuuge poop'])
-    })
-})
+    it('should take the given array and square every number in it', function(){
+        expect(fp.mapBind(moreNums, n => n*n)).toEqual([49,64,81]);
+    });
+});
+
+describe('fp.mapBind', function(){
+    it('should return null if the array given isn\'t all numbers', function(){
+        expect(fp.mapBind(shitNums, n => n*n)).toBe(null);
+    });
+});
 
 describe('fp.mapApply', function(){
-    it('should square every number in the original array', function (){
-        expect(fp.mapApply([1,2,3], ).toEqual([1,4,9]);
-    })
-})
+    it('should take the given array and square every number in it', function(){
+        expect(fp.mapApply(moreNums, [n => n*n])).toEqual([49,64,81]);
+    });
+});
 
 describe('fp.mapApply', function(){
-    it('should add a space and poop to every string', function (){
-        expect(fp.mapApply(['smelly', 'stinky', 'huuuuuge'], [n => `${n} poop`])).toEqual(['smelly poop', 'stinky poop', 'huuuuuge poop'])
-    })
-})
+    it('should return null if the array given isn\'t all numbers', function(){
+        expect(fp.mapApply(shitNums, [n => n*n])).toBe(null);
+    });
+});
+
+
+// filter tests 
 
 describe('fp.filterCall', function(){
-    it('should take an array and return a new array with only the even numbers', function(){
-        expect(fp.filterCall([2,3,4,5,6], n => return n%2 ===0 ? ))
-    })
+    it('should take an array and return to you just the even numbers', function(){
+        expect(fp.filterCall(goodNums, n => n%2 ===0)).toEqual([2,4,6]);
+    });
+});
+
+describe('fp.filterCall', function(){
+    it('should return null if the array given isn\'t all numbers', function(){
+        expect(fp.filterCall(shitNums, n => n%2 ===0)).toBe(null);
+    });
+});
+
+describe('fp.filterBind', function(){
+    it('should take an array and return to you just the even numbers', function(){
+        expect(fp.filterBind(goodNums, n => n%2 ===0)).toEqual([2,4,6]);
+    });
+});
+
+describe('fp.filterBind', function(){
+    it('should return null if the array given isn\'t all numbers', function(){
+        expect(fp.filterBind(shitNums, n => n%2 ===0)).toBe(null);
+    });
+});
+
+describe('fp.filterApply', function(){
+    it('should take an array and return to you just the even numbers', function(){
+        expect(fp.filterApply(goodNums, [n => n%2 ===0])).toEqual([2,4,6]);
+    });
+});
+
+describe('fp.filterApply', function(){
+    it('should return null if the array given isn\'t all numbers', function(){
+        expect(fp.filterApply(shitNums, [n => n%2 ===0])).toBe(null);
+    });
 });
