44c7a165f871a95ca92796993ae997f6dce324c1
jrzollin <jrzollin@yahoo.com>
2017-09-23 15:56:40 -0700

    halfway done

--- lab-james/lib/fp.js
+++ lab-james/lib/fp.js
@@ -2,6 +2,8 @@
 
 let functions = module.exports = {};
 
+//Map=============================================================================================
+
 functions.mapCall = function(array, callback){
 
   return Array.prototype.map.call(array, callback);
@@ -10,6 +12,41 @@ functions.mapCall = function(array, callback){
 
 functions.mapApply = function(array, callback){
 
+  callback = Array.isArray(callback) ? callback : [callback];
+
   return Array.prototype.map.apply(array, callback);
 
 };
+
+let mapArray = [1, 2, 3];
+let mapCB = n => n * 2;
+
+functions.mapBind = function(){
+
+  return Array.prototype.map.call(this.array, this.callback);
+
+}.bind({array: mapArray, callback: mapCB});
+
+//Filter==========================================================================================
+
+functions.filterCall = function(array, callback){
+
+  return Array.prototype.filter.call(array, callback);
+
+};
+
+functions.filterApply = function(array, callback){
+
+  callback = Array.isArray(callback) ? callback : [callback];
+
+  return Array.prototype.filter.apply(array, callback);
+};
+
+let filterArray = [1, 2, 3];
+let filterCB = n => n < 3;
+
+functions.filterBind = function(){
+
+  return Array.prototype.filter.call(this.array, this.callback);
+
+}.bind({array: filterArray, callback: filterCB});
--- lab-james/test/fuctions.test.js
+++ lab-james/test/fuctions.test.js
@@ -3,14 +3,78 @@
 const fp = require('../lib/fp.js');
 const expect = require('expect');
 
-it('mapCall 1: calls map correctly', function(){
+describe('mapCall', function(){
 
-  expect(fp.mapCall([1,2,3], n => n*2)).toEqual([2,4,6]);
+  it('mapCall 1: calls map correctly', function(){
+
+    expect(fp.mapCall([1, 2, 3], n => n * 2)).toEqual([2, 4, 6]);
+
+  });
+
+  it('mapCall 2: calls map correctly', function(){
+
+    expect(fp.mapCall(['red', 'blue', 'green'], index => index.toUpperCase())).toEqual(['RED', 'BLUE', 'GREEN']);
+
+  });
 
 });
 
-it('mapCall 2: calls map correctly', function(){
+//====================================================================================================
 
-  expect(fp.mapCall(['red', 'blue', 'green'], index => index.toUpperCase())).toEqual(['RED', 'BLUE', 'GREEN']);
+describe('mapApply', function(){
+
+  it('mapApply 1: applies map correctly', function(){
+
+    expect(fp.mapApply([1, 2, 3], n => n * 2)).toEqual([2, 4, 6]);
+
+  });
+
+  it('mapApply 2: applies map correctly', function(){
+
+    expect(fp.mapApply(['red', 'blue', 'green'], index => index.toUpperCase())).toEqual(['RED', 'BLUE', 'GREEN']);
+
+  });
+
+});
+
+//====================================================================================================
+
+describe('mapBind', function(){
+
+  it('mapBind 1: binds correctly', function(){
+
+    expect(fp.mapBind()).toEqual([2, 4, 6]);
+
+  });
+
+});
+
+//====================================================================================================
+
+describe('filterCall', function(){
+
+  it('filterCall 1: calls filter correctly', function(){
+
+    expect(fp.filterCall([1, 2, 3], n => n < 3)).toEqual([1, 2]);
+
+  });
+
+  it('filterCall 2: calls filter correctly', function(){
+
+    expect(fp.filterCall(['Red', 'Blue', 'Green'], word => word.length > 3)).toEqual(['Blue', 'Green']);
+
+  });
+
+});
+
+//====================================================================================================
+
+describe('filterBind', function(){
+
+  it('filterBind binds correctly', function(){
+
+    expect(fp.filterBind()).toEqual([1, 2]);
+
+  });
 
 });
