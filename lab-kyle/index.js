f35acbf3fa83631c31139cf5632dad8ffa6a2cca
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-24 19:36:52 -0700

    comments to get started

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -1,1 +1,4 @@
 'use strict';
+
+const fp = require(__dirname + '/lib/fp.js');
+const handler = require(__dirname + '/lib/handler.js');
---
+++ lab-kyle/lib/fp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let fp = module.exports = {};
+
+// Map Function
+// Filter Function
+// Reduce Function
+// Concat Function
+// Splice Function
