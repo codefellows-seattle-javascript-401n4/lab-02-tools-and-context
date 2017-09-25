1499fa3bd6bb669bdc68a68283b9cae3cf5eb639
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-24 20:12:19 -0700

    set up functions to be filled in

--- lab-kyle/lib/fp.js
+++ lab-kyle/lib/fp.js
@@ -1,9 +1,47 @@
 'use strict';
 
-let fp = module.exports = {};
+const fp = module.exports = {};
 
 // Map Function
+
+fp.newMapCall = function (list, callback){
+  return Array.prototype.map.call(list, callback);
+};
+
+fp.newMapApply = function (list, callback){
+  return Array.prototype.map.apply(list, [callback]);
+};
+
+fp.newMapBind = function (){};
+
 // Filter Function
+
+fp.newFilterCall = function (){};
+
+fp.newFilterApply = function (){};
+
+fp.newFilterBind = function (){};
+
 // Reduce Function
+
+fp.newReduceCall = function (){};
+
+fp.newReduceApply = function (){};
+
+fp.newReduceBind = function (){};
+
 // Concat Function
+
+fp.newConcatCall = function (){};
+
+fp.newConcatApply = function (){};
+
+fp.newConcatBind = function (){};
+
 // Splice Function
+
+fp.newSpliceCall = function (){};
+
+fp.newSpliceApply = function (){};
+
+fp.newSpliceBind = function (){};
