326ca465d01169c696193fbf3813ed3d44065f5c
Kyle Richey <richey.kyle.i@gmail.com>
2017-10-30 16:34:08 -0700

    uppercase function working

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -4,22 +4,15 @@ const fp = require('./lib/fp.js');
 // const handler = require('../lib/handler.js');
 
 let capitalize = function(){
-  console.log('in function');
   let commandArgs = process.argv.splice(2, process.argv.length);
 
   if(commandArgs.length === 0){
     return console.log('need more arguments');
   } else {
-
-    console.log('Before Map: ' + commandArgs);
-//WHY IS THIS NOT WORKING?
-
-    fp.newMapApply(commandArgs, (arg) => {
-      arg.toUpperCase();
-      console.log('in map');
+    let upperCased = fp.newMapApply(commandArgs, function(arg) {
+      return arg.toUpperCase();
     });
-    console.log('After Map: ' + commandArgs);
+    console.log(upperCased);
   }
 };
-
 capitalize();
