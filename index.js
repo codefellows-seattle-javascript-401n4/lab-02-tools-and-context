4086294942d6c31ca54451ac3d898a41e2dee617
Maks <pvl1720@gmail.com>
2017-11-14 15:36:19 -0800

    mapBind from examples

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-// 'use strict';
-//
-// const fp = require('/.lib/fp.js');
+'use strict';
+
+const fp = require('/.lib/fp.js');
+
+let numbers = [1,2,3,4,5];
+let myCallback = n => n*5;
+
+console.log(fp.mapCall(numbers,myCallback));
+console.log(fp.mapApply(numbers,[myCallback]));
